ab4510f87c18ff6510c6f5fe98efd402235dcf11
lhybdv <39660787@qq.com>
2024-12-19 22:18:08 +0800

    fix: change mode path to pretained_weights

--- src/models/dwpose/dwpose_detector.py
+++ src/models/dwpose/dwpose_detector.py
@@ -62,7 +62,7 @@ class DWposeDetector:
             return pose
 
 dwpose_detector = DWposeDetector(
-    model_det="/nas2/zhiquan/model/DWPose/yolox_l.onnx",
-    model_pose="/nas2/zhiquan/model/DWPose/dw-ll_ucoco_384.onnx",
+    model_det="./pretrained_weights/dwpose/yolox_l.onnx",
+    model_pose="./pretrained_weights/dwpose/dw-ll_ucoco_384.onnx",
     device=device)
 print('dwpose_detector init ok', device)
